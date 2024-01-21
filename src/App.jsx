2bff7fe2e1363a8f0f48804dacbf4580af62ace0
BayLadyCoder <rachada_32@hotmail.com>
2024-01-21 18:52:27 -0500

    Init project with vite, react, and mui

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function ButtonUsage() {
+  return (
+    <div>
+      <Box sx={{ borderBottom: '1px solid #dddddd' }}>
+        <Typography variant='h5' gutterBottom>
+          &#129299; Readdit
+        </Typography>
+      </Box>
+    </div>
+  );
+}
