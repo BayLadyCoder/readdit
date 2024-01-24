53cb0af4a06d2f67a91f52f3dcb6c72ca53652e0
BayLadyCoder <rachada_32@hotmail.com>
2024-01-24 18:35:03 -0500

    Add PostFormModal to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import * as React from 'react';
+import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import PostForm from './components/PostForm/PostForm';
+
 import Feed from './pages/Feed';
 
 export default function ButtonUsage() {
   return (
-    <div>
-      <Box sx={{ borderBottom: '1px solid #dddddd' }}>
+    <Box>
+      <Box sx={{ borderBottom: '1px solid #dddddd', width: '100%' }}>
         <Typography variant='h5' gutterBottom>
           &#129299; Readdit
         </Typography>
       </Box>
-      <Feed />
-    </div>
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <PostForm />
+        <Feed />
+      </Container>
+    </Box>
   );
 }
