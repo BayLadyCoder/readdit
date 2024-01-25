9fd108189f0a720f3fd238fd51b700657e1206d9
BayLadyCoder <rachada_32@hotmail.com>
2024-01-24 19:27:53 -0500

    Use MUI Card for Post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,99 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const Post = ({ post }) => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div key={post.id}>
+    <Card sx={{ width: { xs: 345, sm: 500, md: 600 } }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
+            {post.author.name[0]}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label='settings'>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={post.title}
+        subheader={post.createdAt}
+      />
+      <CardMedia
+        component='img'
+        height='194'
+        image='/images/img2.jpeg'
+        alt='Sample image'
+      />
+      <CardContent>
+        <Typography variant='body2' color='text.secondary'>
+          {post.content}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label='add to favorites'>
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label='share'>
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label='show more'
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout='auto' unmountOnExit>
+        <CardContent>
+          {/* <Typography paragraph>Content:</Typography> */}
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            corporis consectetur corrupti unde pariatur ut itaque autem tempora
+            ab ex necessitatibus animi sunt amet asperiores iusto quae
+            laudantium, perferendis odit.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+};
+
+const PostOld = ({ post }) => {
+  return (
+    <div>
       <h3>{post.title}</h3>
       <p>{post.content}</p>
       <p>{post.author.name}</p>
