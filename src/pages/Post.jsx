3e5b2436128436453a8570d6ba38ae6724f67be4
BayLadyCoder <rachada_32@hotmail.com>
2024-01-26 18:49:13 -0500

    Get post data for Post page

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../customHooks/useFetch';
+import { createGetOnePostURL } from '../resources/URLs.js';
 
 const Post = () => {
-  return <div>Post Page</div>;
+  const { postId } = useParams();
+  const { data, isLoading, isError } = useFetch(createGetOnePostURL(postId));
+
+  // todo: style display
+
+  if (isError) return <h1>Failed to load</h1>;
+  if (isLoading) return <h1>Loading...</h1>;
+  if (!data) return <h1>No post found</h1>;
+
+  return (
+    <div>
+      <h1>{data.post.title}</h1>
+      <p>Author: {data.post.content}</p>
+      <p>{data.post.content}</p>
+    </div>
+  );
 };
 
 export default Post;
--- src/resources/URLs.js
+++ src/resources/URLs.js
@@ -1,3 +1,5 @@
 const baseURL = 'http://localhost:8080';
 
 export const getAllPostsURL = `${baseURL}/api/posts`;
+
+export const createGetOnePostURL = (postId) => `${baseURL}/api/posts/${postId}`;
