1b9ad47a969e0c82478afeeaa57ab71959bba0e6
BayLadyCoder <rachada_32@hotmail.com>
2024-02-01 17:11:46 -0500

    Move Edit button and add Delete button to MenuItems

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
@@ -12,19 +11,61 @@ import { timeAgo } from '../../helpers/timeAgo';
 import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
 
-const PostCard = ({ post, setOpenPostForm }) => {
+const PostCard = ({ post }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Card sx={{ width: { xs: 345, sm: 500, md: 600 } }}>
       <CardHeader
         action={
-          <IconButton aria-label='settings'>
+          <IconButton
+            aria-label='settings'
+            id='basic-button'
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup='true'
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
             <MoreVertIcon />
           </IconButton>
         }
         title={post.title}
         subheader={`Posted by ${post.author.name} ${timeAgo(post.updatedAt)}`}
       />
+      <Menu
+        id='basic-menu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <Link
+          to={`posts/${post._id}/edit`}
+          style={{ textDecoration: 'none', color: 'inherit' }}
+        >
+          <MenuItem onClick={handleClose}>
+            <EditIcon sx={{ mr: '5px' }} size='small' />
+            Edit
+          </MenuItem>
+        </Link>
+        <MenuItem onClick={handleClose}>
+          <DeleteOutlinedIcon sx={{ mr: '5px' }} size='small' />
+          Delete
+        </MenuItem>
+      </Menu>
       <Link to={`/posts/${post._id}`}>
         <CardMedia
           component='img'
@@ -34,7 +75,7 @@ const PostCard = ({ post, setOpenPostForm }) => {
         />
       </Link>
 
-      <CardActions disableSpacing>
+      <CardActions>
         <IconButton aria-label='add to favorites' size='small'>
           <ModeCommentOutlinedIcon sx={{ mr: '5px' }} />
           58 Comments
@@ -45,9 +86,6 @@ const PostCard = ({ post, setOpenPostForm }) => {
         <IconButton aria-label='share' size='small'>
           <ShareOutlinedIcon sx={{ mr: '5px' }} /> Share
         </IconButton>
-        <Link to={`posts/${post._id}/edit`}>
-          <Button onClick={() => setOpenPostForm(true)}>Edit</Button>{' '}
-        </Link>
       </CardActions>
     </Card>
   );
