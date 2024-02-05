496590a9524f2a6396aa6b34a4aa8181b6123412
BayLadyCoder <rachada_32@hotmail.com>
2024-02-05 16:52:09 -0500

    Remove unused variables

--- src/pages/PostForm.jsx
+++ src/pages/PostForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
