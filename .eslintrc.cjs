215337f7663a8c9b2e66e9b83a2dc3398cb14fe1
BayLadyCoder <rachada_32@hotmail.com>
2024-02-05 16:35:23 -0500

    Update ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,42 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['react', 'react-hooks', 'react-refresh'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: '18.2', // React version. "detect" automatically picks the version you have installed.
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,jsx,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+
   rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
