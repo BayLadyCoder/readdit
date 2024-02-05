5e08e07a79f619be2b2f9ffe7f1922aefffffe68
BayLadyCoder <rachada_32@hotmail.com>
2024-02-05 13:55:40 -0500

    Fix useFetch bug for POST and DELETE requests, improve error handling

--- src/customHooks/useFetch.jsx
+++ src/customHooks/useFetch.jsx
@@ -1,40 +1,68 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
-export const useFetch = ({
-  url,
-  dataHandler,
-  options = {},
-  immediate = true,
-}) => {
-  if (!url) {
-    console.error('url is required for useFetch');
-    return {};
-  }
+const initialUseFetchParameters = {
+  url: undefined,
+  dataHandler: undefined,
+  options: {},
+  immediate: true,
+};
 
+export const useFetch = ({
+  url: defaultUrl,
+  dataHandler: defaultDataHandler,
+  options: defaultOptions = {},
+  immediate = true,
+} = initialUseFetchParameters) => {
   const [data, setData] = useState(undefined);
   const [isError, setIsError] = useState(false);
   const [error, setError] = useState(undefined);
   const [isLoading, setIsLoading] = useState(false);
   const token = useSelector((state) => state.user.token);
 
-  const headers = {
-    Authorization: `Bearer ${token}`,
-  };
+  const fetchData = useCallback(
+    async (
+      {
+        url = defaultUrl,
+        options = defaultOptions,
+        dataHandler = defaultDataHandler,
+      } = {
+        url: defaultUrl,
+        options: defaultOptions,
+        dataHandler: defaultDataHandler,
+      }
+    ) => {
+      if (!url) {
+        console.error('URL is required for useFetch');
+        return {};
+      }
 
-  const fetchData = useCallback(async () => {
-    setIsError(false);
-    setIsLoading(true);
-    try {
-      const res = await fetch(url, { headers, ...options });
-      const data = await res.json();
-      dataHandler ? dataHandler(data) : setData(data);
-    } catch (error) {
-      setIsError(true);
-      setError(err);
-    }
-    setIsLoading(false);
-  }, [url, dataHandler, options, immediate]);
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const res = await fetch(url, {
+          headers: { Authorization: `Bearer ${token}`, ...options.headers },
+          ...options,
+        });
+
+        const data = await res.json();
+
+        if (res.status >= 400) {
+          setIsError(true);
+          setError(data.message);
+          throw new Error(data.message);
+        }
+
+        dataHandler ? dataHandler(data) : setData(data);
+      } catch (error) {
+        console.error({ error });
+        setIsError(true);
+        setError(error);
+      }
+      setIsLoading(false);
+    },
+    [defaultUrl, defaultDataHandler, defaultOptions, immediate]
+  );
 
   useEffect(() => {
     if (immediate) {
