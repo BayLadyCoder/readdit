205ab16ded72c36d41d0d7cfbc99a25ee81475fd
BayLadyCoder <rachada_32@hotmail.com>
2024-02-08 17:40:44 -0500

    Remove unused code

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useFetch } from '../customHooks/useFetch';
 import { createPostURLByPostId } from '../resources/URLs.js';
