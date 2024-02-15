97b253f199f055d9dc8c4c2dfc6914d27095c9ba
BayLadyCoder <rachada_32@hotmail.com>
2024-02-14 19:35:30 -0500

    Add avatar and more menu to UserNavMenu

--- src/components/UserNavMenu/UserNavMenu.jsx
+++ src/components/UserNavMenu/UserNavMenu.jsx
@@ -2,9 +2,19 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
+import { deepOrange, grey } from '@mui/material/colors';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Switch from '@mui/material/Switch';
+import Typography from '@mui/material/Typography';
+
 import { logout } from '../../reducers/userSlice';
 
 const UserNavMenu = () => {
@@ -21,14 +31,29 @@ const UserNavMenu = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <Stack>
       <Button
+        sx={{ border: 'solid 1px #eee', color: '#000', textTransform: 'none' }}
         id='basic-button'
         aria-controls={open ? 'basic-menu' : undefined}
         aria-haspopup='true'
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+        variant='string'
       >
+        <Avatar
+          sx={{
+            bgcolor: deepOrange[500],
+            fontSize: '13px',
+            width: 30,
+            height: 30,
+            mr: '5px',
+          }}
+        >
+          {user.avatarLabel}
+        </Avatar>
+
         {user.username}
       </Button>
       <Menu
@@ -36,6 +61,8 @@ const UserNavMenu = () => {
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        sx={{ left: '-15px' }}
         MenuListProps={{
           'aria-labelledby': 'basic-button',
         }}
@@ -44,19 +71,49 @@ const UserNavMenu = () => {
           to='/profile'
           style={{ textDecoration: 'none', color: 'inherit' }}
         >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem
+            align='center'
+            sx={{ width: '200px' }}
+            onClick={handleClose}
+          >
+            <AccountCircleIcon bgcolor={grey[700]} sx={{ mr: '10px' }} />{' '}
+            <Typography sx={{ fontSize: '0.95rem' }}>Profile</Typography>
+          </MenuItem>
         </Link>
-        <MenuItem onClick={handleClose}>Setting</MenuItem>
         <MenuItem
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontSize: '0.95rem',
+          }}
+        >
+          <Typography sx={{ fontSize: '0.95rem' }}>Online Status</Typography>
+          <Switch />
+        </MenuItem>
+        <MenuItem
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontSize: '0.95rem',
+          }}
+        >
+          Dark Theme
+          <Switch />
+        </MenuItem>
+        <Divider />
+        <MenuItem
+          sx={{
+            fontSize: '0.95rem',
+          }}
           onClick={() => {
             dispatch(logout());
             navigate('/');
           }}
         >
-          Logout
+          <LogoutIcon sx={{ mr: '10px' }} /> Logout
         </MenuItem>
       </Menu>
-    </div>
+    </Stack>
   );
 };
 
