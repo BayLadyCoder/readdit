67107541546ef11f713a2dfe7d6dfbe678775f28
BayLadyCoder <rachada_32@hotmail.com>
2024-02-19 17:28:40 -0500

    Style Post page

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,6 +1,13 @@
 import { useParams } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
 import { useFetch } from '../customHooks/useFetch';
 import { createPostURLByPostId } from '../resources/URLs.js';
+import { baseURL } from '../resources/URLs';
+import { timeAgo } from '../helpers/timeAgo';
+import PostActionFooter from '../components/PostActionFooter/PostActionFooter';
 
 const Post = () => {
   const { postId } = useParams();
@@ -16,11 +23,45 @@ const Post = () => {
   if (!data) return <h1>No post found</h1>;
 
   return (
-    <div>
-      <h1>{data.post.title}</h1>
-      <p>Author: {data.post.content}</p>
-      <p>{data.post.content}</p>
-    </div>
+    <Paper
+      sx={{
+        padding: '10px 20px 10px 20px',
+        width: { xs: '90%', sm: '80%', md: '70%' },
+        maxWidth: '900px',
+      }}
+    >
+      <Stack gap={1}>
+        <Typography
+          variant='body2'
+          color='gray'
+          sx={{
+            fontSize: '12px',
+          }}
+        >
+          Posted by : {data.post.author.username} {timeAgo(data.post.createdAt)}
+        </Typography>
+        <Typography variant='h1' sx={{ fontSize: '20px', fontWeight: 500 }}>
+          {data.post.title}
+        </Typography>
+        {data.post.imageUrl && (
+          <img src={baseURL + '/' + data.post.imageUrl} alt='Post image' />
+        )}
+        {data.post.content && (
+          <Typography
+            sx={{
+              p: { sm: '5px 10px', md: '10px 20px' },
+              whiteSpace: 'break-spaces',
+              textAlign: 'justify',
+              fontSize: '14px',
+              fontFamily: 'Noto Sans',
+            }}
+          >
+            {data.post.content}
+          </Typography>
+        )}
+      </Stack>
+      <PostActionFooter />
+    </Paper>
   );
 };
 
