57e753311fcdf18178187683312f7db5908dd190
BayLadyCoder <rachada_32@hotmail.com>
2024-02-19 17:24:45 -0500

    Add new font: Noto Sans

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,6 @@ import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
   typography: {
-    fontFamily: ['IBM Plex Sans', 'sans-serif'].join(','),
+    fontFamily: ['IBM Plex Sans', 'Noto Sans', 'sans-serif'].join(','),
   },
 });
