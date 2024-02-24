993a8588f6f6468539728917620a7bf91b547c46
BayLadyCoder <rachada_32@hotmail.com>
2024-02-23 19:33:45 -0500

    Decouple PostTitle and PostMetaData from PostCardHeader

--- src/components/PostCard/PostCardHeader.jsx
+++ src/components/PostCard/PostCardHeader.jsx
@@ -7,6 +7,22 @@ import { timeAgo } from '../../helpers/timeAgo';
 import { PostContext } from '../../context/PostContext';
 import AuthorMoreActionButton from '../AuthorMoreActionButton/AuthorMoreActionButton.jsx';
 
+const PostTitle = ({ title }) => {
+  return (
+    <Typography sx={{ fontWeight: '500', fontSize: '20px' }}>
+      {title}
+    </Typography>
+  );
+};
+
+const PostMetaData = ({ authorUsername, createdAt }) => {
+  return (
+    <Typography variant='body2' sx={{ fontSize: '12px', color: '#555' }}>
+      {`Posted by ${authorUsername} ${timeAgo(createdAt)}`}
+    </Typography>
+  );
+};
+
 const PostCardHeader = () => {
   const post = useContext(PostContext);
 
@@ -19,12 +35,11 @@ const PostCardHeader = () => {
       alignItems='flex-start'
     >
       <Box>
-        <Typography sx={{ fontWeight: '500', fontSize: '20px' }}>
-          {post.title}
-        </Typography>
-        <Typography variant='body2' sx={{ fontSize: '12px', color: '#555' }}>
-          {`Posted by ${post.author.username} ${timeAgo(post.createdAt)}`}
-        </Typography>
+        <PostMetaData
+          authorUsername={post.author.username}
+          createdAt={post.createdAt}
+        />
+        <PostTitle title={post.title} />
       </Box>
       <AuthorMoreActionButton post={post} forHeader />
     </Stack>
