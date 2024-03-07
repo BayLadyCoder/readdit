e79b506411c883b2842549899839eba70289681c
BayLadyCoder <rachada_32@hotmail.com>
2024-03-07 16:26:05 -0500

    Add delete comment functionality to CommentCard

--- src/components/Comment/CommentCard/CommentCard.jsx
+++ src/components/Comment/CommentCard/CommentCard.jsx
@@ -1,8 +1,23 @@
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import { useFetch } from '../../../customHooks/useFetch';
+import { baseURL } from '../../../resources/URLs.js';
 
 const CommentCard = ({ comment }) => {
+  const { fetchData: deleteComment } = useFetch({
+    immediate: false,
+    options: { method: 'DELETE' },
+  });
+
+  const handleDeleteComment = (e) => {
+    e.preventDefault();
+    deleteComment({ url: `${baseURL}/api/comments/${comment._id}` });
+  };
+
   return (
     <Stack
       sx={{
@@ -18,6 +33,9 @@ const CommentCard = ({ comment }) => {
         <Typography>Comment: {comment.comment}</Typography>
         <Typography>Author: {comment.author.username}</Typography>
         <Typography>Time ago: {comment.createdAt}</Typography>
+        <IconButton onClick={handleDeleteComment}>
+          <DeleteIcon />
+        </IconButton>
       </Paper>
     </Stack>
   );
